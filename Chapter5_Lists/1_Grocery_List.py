9b24c5beb4b73fdad7c10249d4e29308ba940029
sksguswjd717 <pjh752829@gmail.com>
2025-12-14 11:54:43 +0900

    Complited Chapter 5-1,2

---
+++ Chapter5_Lists/1_Grocery_List.py
@@ -0,0 +1,13 @@
+# List items allow duplicate values.
+# Lists can have values with different data types.
+# There's no limit to how much data a list can hold.
+
+grocery = ['🥚 Eggs',
+'🥑 Avocados',
+'🍪 Cookies',
+'🌶 Hot Pepper Jam',
+'🫐 Blueberries',
+'🥦 Broccoli']
+
+# Lists can have values with different data types!!
+print(grocery)
---
+++ Chapter5_Lists/2_To-Do List.py
@@ -0,0 +1,41 @@
+# # Write code below 💖
+# # Index
+# vowels = ['a', 'e', 'i', 'o', 'u']
+# # Index:   0    1    2    3    4
+# print(vowels[0])     # Output: a
+# print(vowels[1])     # Output: e
+# print(vowels[2])     # Output: i
+# print(vowels[3])     # Output: o
+# print(vowels[4])     # Output: u
+
+
+# # Negative Index
+# vowels = ['a', 'e', 'i', 'o', 'u']
+# # Index:   0    1    2    3    4
+# # Index:  -5   -4   -3   -2   -1
+
+
+# # Slicing vowels[a,b] -> show all of items before index b
+# vowels = ['a', 'e', 'i', 'o', 'u']
+
+# print(vowels[0 : 3])
+# print(vowels[1 : 3])
+
+# # Output:
+# # ['a', 'e', 'i']
+# # ['e', 'i']
+
+todo = ['🏦 Get quarters.',
+'🧺 Do laundry.',
+'🌳 Take a walk.',
+'💈 Get a haircut.',
+'🍵 Make some tea.',
+'💻 Complete Lists chapter.',
+'💖 Call mom.',
+'📺 Watch My Hero Academia.']
+
+
+print(todo[0])
+print(todo[1])
+
+print(todo[3:6])
