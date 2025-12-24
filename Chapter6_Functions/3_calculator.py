7024e0fb62faee2b0b49ca07964ea7e5881a56cd
sksguswjd717 <pjh752829@gmail.com>
2025-12-24 20:47:14 +0900

    Chapter6 Function and return

---
+++ Chapter6_Functions/3_calculator.py
@@ -0,0 +1,20 @@
+def add(a,b):
+    answer = a+b
+    return answer
+
+def subtract(a,b):
+    answer = a-b
+    return answer
+
+def multiply(a,b):
+    answer = a*b
+    return answer
+
+def divide(a,b):
+    answer = a/b
+    return answer
+
+def exp(a,b):
+    answer = a**b
+    return answer
+
